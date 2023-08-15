03e15025c6899d5d4314b00d4e2216203c623c14
ajou20658 <kwy1379@ajou.ac.kr>
2023-08-15 23:09:02 +0900

    기사 검색

--- src/main/java/com/example/demo/service/CrawlService.java
+++ src/main/java/com/example/demo/service/CrawlService.java
@@ -124,8 +124,8 @@ public class CrawlService {
 
     public void keyWordCrawling(String keyword) throws Exception{
         String url= "https://search.naver.com/search.naver?where=news&query="+keyword+"&sm=tab_opt&sort=0&photo=0&field=0&pd=0&ds=&de=&docid=&related=0&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so%3Ar%2Cp%3Aall&is_sug_officeid=0";
-        Elements elements = Jsoup.connect(url).get().select("#main_pack > section > div > div.group_news > ul");
-        for(Element e:elements.select("li")){
+        Elements elements = Jsoup.connect(url).get().select("#main_pack > section > div > div.group_news > ul > li");
+        for(Element e:elements){
 
             Elements li = e.select("div.news_wrap.api_ani_send > div > div.news_info > div.info_group");
             if(li.text().contains("네이버 뉴스")){
