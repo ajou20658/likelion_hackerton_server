2d315f7ef580e44e90d41faae0cebf7a2e6dea58
ajou20658 <kwy1379@ajou.ac.kr>
2023-08-17 17:45:07 +0900

    크롤링 코드 수정

--- src/main/java/com/example/demo/config/AsyncConfig.java
+++ src/main/java/com/example/demo/config/AsyncConfig.java
@@ -7,9 +7,9 @@ public class AsyncConfig {
     @Bean
     public ThreadPoolTaskExecutor taskExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(5);  // 예: 최소 스레드 개수
-        executor.setMaxPoolSize(10);  // 예: 최대 스레드 개수
-        executor.setQueueCapacity(20); // 예: 대기 큐 크기
+        executor.setCorePoolSize(1);  // 예: 최소 스레드 개수
+        executor.setMaxPoolSize(5);  // 예: 최대 스레드 개수
+        executor.setQueueCapacity(10); // 예: 대기 큐 크기
         executor.setThreadNamePrefix("MyThreadPool-"); // 스레드 이름 접두사
         executor.initialize();
         return executor;
