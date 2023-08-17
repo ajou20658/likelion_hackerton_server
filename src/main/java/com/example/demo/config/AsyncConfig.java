0c85c8b29453a9068a2837cda204f2a65d3a3f4b
ajou20658 <kwy1379@ajou.ac.kr>
2023-08-17 20:38:35 +0900

    요약업데이트

--- src/main/java/com/example/demo/config/AsyncConfig.java
+++ src/main/java/com/example/demo/config/AsyncConfig.java
@@ -1,17 +1,17 @@
-package com.example.demo.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-
-public class AsyncConfig {
-    @Bean
-    public ThreadPoolTaskExecutor taskExecutor() {
-        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(1);  // 예: 최소 스레드 개수
-        executor.setMaxPoolSize(1);  // 예: 최대 스레드 개수
-        executor.setQueueCapacity(1); // 예: 대기 큐 크기
-        executor.setThreadNamePrefix("MyThreadPool-"); // 스레드 이름 접두사
-        executor.initialize();
-        return executor;
-    }
-}
+//package com.example.demo.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+//
+//public class AsyncConfig {
+//    @Bean
+//    public ThreadPoolTaskExecutor taskExecutor() {
+//        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+//        executor.setCorePoolSize(1);  // 예: 최소 스레드 개수
+//        executor.setMaxPoolSize(1);  // 예: 최대 스레드 개수
+//        executor.setQueueCapacity(1); // 예: 대기 큐 크기
+//        executor.setThreadNamePrefix("MyThreadPool-"); // 스레드 이름 접두사
+//        executor.initialize();
+//        return executor;
+//    }
+//}
