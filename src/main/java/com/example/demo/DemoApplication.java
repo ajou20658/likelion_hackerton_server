b16cec4f7df94ed11ff1e49a42b117cce6fc776e
ajou20658 <kwy1379@ajou.ac.kr>
2023-08-21 16:39:36 +0900

    index.html 작성

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,14 +2,39 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+import java.util.Arrays;
 
 @EnableMongoRepositories("com.example.demo.repository")
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		var context = SpringApplication.run(DemoApplication.class, args);
+
+		String[] beanNames = context.getBeanDefinitionNames();
+		for(String beanName: beanNames){
+			System.out.println("beanName = " + beanName);
+		}
+		context.close();
 	}
 
 }
+
+//@Configuration
+//@ComponentScan
+//public class LazyInitializationLauncherApplication{
+//	public static void main(String[] args){
+//		try(var context =
+//				new AnnotationConfigApplicationContext(
+//						LazyInitializationLauncherApplication.class
+//				)){
+//			Arrays.stream(context.getBeanDefinitionNames())
+//					.forEach(System.out::println);
+//		}
+//	}
+//}
